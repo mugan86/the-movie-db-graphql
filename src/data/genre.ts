e084241108837f138cf3bae9a8abbc4a0862eb17
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-14 08:59:06 +0200

    Add datasource to take genres data

---
+++ src/data/genre.ts
@@ -0,0 +1,14 @@
+import { TheMovieDB } from "./the-movie-db";
+
+class Genre extends TheMovieDB {
+    async list(typeList: string) {
+        console.log(`genre/${typeList}/list?language=${this.selectLanguage}`);
+        return await this.get(`genre/${typeList}/list?language=${this.selectLanguage}`, {
+            cacheOptions: { ttl: 60 }
+        }).then((result: {genres: Array<object>}) => {
+            return result.genres
+        });
+    }
+}
+
+export default Genre;
