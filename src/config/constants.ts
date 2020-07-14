50fca9477046961ff704387f44d9d5265f4ba7c8
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-14 08:53:26 +0200

    Add info select language configuration

---
+++ src/config/constants.ts
@@ -0,0 +1,18 @@
+/**
+ * API Languages options.
+ * Define with reference https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+ * (Take ISO_639-1)
+ */
+export enum LANGUAGES {
+    CHINESE = "zh",
+    ENGLISH = "en",
+    FRENCH = "fr",
+    GERMAN = "de",
+    GREEK = "el",
+    ITALIAN = "it",
+    JAPANESE = "ja",
+    POLISH = "pl",
+    PORTUGUESE = "pt",
+    RUSSIAN = "ru",
+    SPANISH = "es",
+}
--- src/data/the-movie-db.ts
+++ src/data/the-movie-db.ts
@@ -1,12 +1,15 @@
 import { RESTDataSource, RequestOptions } from "apollo-datasource-rest";
+import { LANGUAGES } from "../config/constants";
 
 /**
  * All API documentation find: https://developers.themoviedb.org/3/getting-started/introduction
  */
 export class TheMovieDB extends RESTDataSource {
-    constructor() {
+    selectLanguage: string;
+    constructor(selectLanguage: string = LANGUAGES.SPANISH) {
         super();
         this.baseURL = "https://api.themoviedb.org/3/";
+        this.selectLanguage = selectLanguage;
     }
 
     willSendRequest(request: RequestOptions) {
