1442b63fe12dbfaa75b0b7ae72935771f61489cd
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-15 08:49:39 +0200

    Remove build

--- build/resolvers/query/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const genre_1 = __importDefault(require("./genre"));
-const discover_1 = __importDefault(require("./discover"));
-const queryResolvers = Object.assign(Object.assign({}, genre_1.default), discover_1.default);
-exports.default = queryResolvers;
